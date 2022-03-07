cea697f41c8f5c9e4ba93cb7dc698d3427e1ad43
lymine1996 <lymine1996@163.com>
2022-03-07 10:00:26 -0600

    Add support page and links to the other tools

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-const Nav = () => {
+const NavBar = () => {
 	return (
 		<Navbar bg="dark" variant="dark" fixed="top">
 			<Navbar.Brand href="#home">
-				<Link to="/" style={{ color: 'white', textDecoration: 'None' }}>
+				<NavLink to="/" style={{ color: 'white', textDecoration: 'None' }}>
 					Conservation Visualization Tool
-				</Link>
+				</NavLink>
 			</Navbar.Brand>
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<NavLink to="/" className="ml-3 mt-2" style={{ color: 'white'}}>
+						Map
+					</NavLink>
+					<NavLink to="/help" className="ml-3 mt-2" style={{ color: 'white'}}>
+						Support
+					</NavLink>
+					<NavDropdown title="More" className="ml-3">
+					<NavDropdown.Item
+						href="https://www.quest.fwrc.msstate.edu/sca/about-the-project.php"
+						target="_blank"
+					>
+						Strategic Conservation Assessment (SCA) Project
+					</NavDropdown.Item>
+					<NavDropdown.Divider />
+					<NavDropdown.Item
+						href="http://sca-cit.herokuapp.com/"
+						target="_blank"
+					>
+						Conservation Planning Inventory Tool (CIT)
+					</NavDropdown.Item>
+					<NavDropdown.Item
+						href="http://sca-cpt-frontend.herokuapp.com/"
+						target="_blank"
+					>
+						Conservation Prioritization Tool (CPT)
+					</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	);
 };
 
-export default Nav;
+export default NavBar;
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Route,Switch,Redirect} from 'react-router-dom';
 import Main from './Main';
+import Help from "./Help";
 
 const Routes = () =>{
     return (
         <Switch>
             <Route exact path="/">
                 <Main/>
+            </Route>
+            <Route exact path="/help">
+                <Help />
             </Route>
             <Route>
                 <Redirect to="/"/>
