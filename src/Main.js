f0d85e932c61219c3c5bb22005dd252e73986e0a
lymine1996 <lymine1996@163.com>
2022-03-20 15:00:04 -0500

    Enable snapshot of the current map view

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { RiScreenshot2Fill } from "react-icons/ri";
+import ControlPanel from "./ControlPanel";
+import Sidebar from "./Sidebar";
 import Map from "./Map";
 import "./main.css";
-import Sidebar from "./Sidebar";
-import ControlPanel from "./ControlPanel";
 
 const Main = () => {
   const [activeSidebar, setActiveSidebar] = useState(false);
   const [data, setData] = useState(null);
   const [hoverInfo, setHoverInfo] = useState(null);
   const [weightsDone, setWeightsDone] = useState(false);
+  const [show, setShow] = useState(false);
+  const [imageURL, setImageURL] = useState(null);
+  const mapRef = useRef();
+  
+  const handleClose = () => setShow(false);
+
+  const handleShow = () => setShow(true);
+
+  const resizeImageURL = (url, newWidth, newHeight) => {
+    return new Promise(async function(resolve, reject){
+      var img = document.createElement('img');
+      img.onload = function() {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        ctx.drawImage(this, 0, 0, newWidth, newHeight);
+        var dataURI = canvas.toDataURL();
+        resolve(dataURI);
+      };
+      img.src = url;
+    });
+  };
+
+  const getImage = async () => {
+    var originalImage = mapRef.current.getMap().getCanvas().toDataURL();
+    var resizedImage = await resizeImageURL(originalImage, 750, 500);
+    setImageURL(resizedImage);
+    handleShow();
+  };
 
   return (
     <div>
@@ -42,11 +73,33 @@ const Main = () => {
           weightsDone={weightsDone}
           data={data}
           setHoverInfo={setHoverInfo}
+          setImageURL={setImageURL}
+          mapRef={mapRef}
         />
         <ControlPanel
           id="popupWindow"
           hoverInfo={hoverInfo ? hoverInfo : { hexagon: {} }}
         ></ControlPanel>
+        <Button
+          style={{ position: "fixed", top: "200px", right: "20px", width: "300px", zIndex: 1 }}
+          variant="secondary"
+          onClick={getImage}
+        >
+          <RiScreenshot2Fill /> &nbsp;
+          Export Current View
+        </Button>
+        <Modal centered show={show} onHide={handleClose} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Current Map View
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="d-flex flex-column justify-content-center">
+              <img src={imageURL} alt={"Current Map View"} />
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
